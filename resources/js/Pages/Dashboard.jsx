6aa7633442ce53f2a40221a754999bdea4207bf1
Arpit Adhikari <arpitadhikari248@gmail.com>
2025-02-04 23:14:12 +0545

    notepad

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -25,9 +25,15 @@ export default function Dashboard({children}) {
                 
                 {/* main content */}
                 <div className='flex-1 p-8'>
-                    {children}
-                </div>
-
+                    <div className='flex-1'>{children}</div>
+                
+            <div className="p-4 bg-light rounded shadow-sm w-100 w-sm-50 w-md-25 w-lg-20 h-auto">
+                <h3 className="mb-2">Important notes</h3>
+                <textarea className="w-100 h-100 p-2 border rounded" 
+                // value={note}
+                placeholder="Write your notes...."/>
+            </div>
+            </div>
             </div>
         </AuthenticatedLayout>
     );
