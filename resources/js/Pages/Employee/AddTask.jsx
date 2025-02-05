21ff078eb8d680c813336ec49256523c0081fa13
sachin magar <gitsachin123@gmail.com>
2025-02-04 21:02:57 -0800

    side bar and add task component

---
+++ resources/js/Pages/Employee/AddTask.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from '../Dashboard';
+const AddTask = () => {
+  return (
+    <Dashboard>
+
+    <div className='flex-1 p-8 bg-slate-500'>
+      chalyo
+    </div>
+    </Dashboard>
+  )
+}
+
+export default AddTask
