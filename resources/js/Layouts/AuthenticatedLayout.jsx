bc2591264def79b2e3ee346f990e6f70ffcc917e
Arpit Adhikari <arpitadhikari248@gmail.com>
2025-02-09 09:32:25 +0545

    layout for all

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -229,14 +229,6 @@ export default function AuthenticatedLayout({ header, children }) {
                 </div>
             </nav>
 
-            {/* {header && (
-                <header className="bg-white shadow dark:bg-gray-800">
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        {header}
-                    </div>
-                </header>
-            )} */}
-
             <main>{children}</main>
         </div>
     );
