82d738e8d926e496b00369285215c4c83aaebf4e
sachin magar <gitsachin123@gmail.com>
2025-02-08 23:53:23 -0800

    user and task relationship established

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,11 +9,18 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['task_name', 'task_type', 'deadline', 'priority_level'];
+    protected $fillable = ['user_id','task_name', 'task_type', 'deadline', 'priority_level'];
 
     // Define the relationship to Report
     public function report()
     {
         return $this->hasOne(Report::class, 'task_id', 'id');
     }
+
+    // Define the relationship to User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
